ce60b3f836fff8f1aad8191a38cf2636221123d7
Kallol Ghosal <kallol@euronoxxgroup.com>
2025-02-12 09:58:48 +0530

    href css updated

--- app/components/TopHeader.js
+++ app/components/TopHeader.js
@@ -1,7 +1,8 @@
 "use client"
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown, Row, Col } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../globals.css';
+import Link from "next/link";
 
 const TopHeader = () => {
     return (
@@ -22,12 +23,56 @@ const TopHeader = () => {
                         </NavDropdown>
                         <Nav.Link href="#">Contact Us</Nav.Link>
                         <NavDropdown title="Support" id="collapsible-nav-dropdown" className="dropdownmg">
-                            <NavDropdown.Item href="#">College Students</NavDropdown.Item>
-                            <NavDropdown.Item href="#">Millitary &amp; Veterans</NavDropdown.Item>
-                            <NavDropdown.Item href="#">Postal Service Workers</NavDropdown.Item>
-                            <NavDropdown.Item href="#">Animal Charities</NavDropdown.Item>
-                            <NavDropdown.Item href="#">Family Plans</NavDropdown.Item>
-                            <NavDropdown.Item href="#">Zoiko Music Hub</NavDropdown.Item>
+                            <Container className="py-4">
+                                <Row>
+                                    <Col md={3} sm={12} xs={12}>
+                                        <p><span className="txtgreen">Customer Support</span></p>
+                                        <ul className="menulist">
+                                            <li><Link href={"/support"}>Help &amp; Support</Link></li>
+                                            <li><Link href={"/reasons-to-love-zoiko-mobile"}>Reasons to love Zoiko</Link></li>
+                                            <li><Link href={"https://ee.co.uk/help/mobile-coverage-checker"} target="_blank">Check Network Coverage</Link></li>
+                                            <li><Link href={"/faq"}>FAQs</Link></li>
+                                            <li><Link href={"/how-to-activate-sim"}>How to activate SIM Cards</Link></li>
+                                        </ul>
+                                    </Col>
+                                    <Col md={2} sm={12} xs={12}>
+                                        <p><span className="txtgreen">Get Started</span></p>
+                                        <ul className="menulist">
+                                            <li><Link href={"/switch"}>Switch &amp; Save</Link></li>
+                                            <li><Link href="/login">Join Zoiko Family</Link></li>
+                                            <li><Link href="/free-delivery-policy">Free Delivery</Link></li>
+                                            <li><Link href="/product-category/refurbished">Refurbished Smartphones</Link></li>
+                                            <li><Link href={"/return-policy"}>Return Policy</Link></li>
+                                        </ul>
+                                    </Col>
+                                    <Col md={2} sm={12} xs={12}>
+                                        <p><span className="txtgreen">Customer Dashbpard</span></p>
+                                        <ul className="menulist">
+                                            <li><Link href="/login">Login</Link></li>
+                                        </ul>
+                                    </Col>
+                                    <Col md={3} sm={12} xs={12}>
+                                        <p><span className="txtgreen">Tariffs &amp; Prices</span></p>
+                                        <ul className="menulist">
+                                            <li><Link href={"/roaming-and-overage"}>Roaming Charges</Link></li>
+                                            <li><Link href={"/bundled-sim-plan-offer"}>Bundled Offers</Link></li>
+                                            <li><Link href={"/free-international-calling"}>Free International Calls</Link></li>
+                                            <li><Link href={"/international-out-of-bundle-rates"}>Out-of-Bundle Rates</Link></li>
+                                            <li><Link href={"/civilservants"}>Public Sector Lifetime Deals</Link></li>
+                                        </ul>
+                                    </Col>
+                                    <Col md={2} sm={12} xs={12}>
+                                        <p><span className="txtgreen">What&apos;s Included</span></p>
+                                        <ul className="menulist">
+                                            <li><Link href={"/5G-speed"}>5G Speed</Link></li>
+                                            <li><Link href={"/wi-fi-calling"}>Wi-Fi Calling</Link></li>
+                                            <li><Link href={"/eu-roaming"}>EU Roaming</Link></li>
+                                            <li><Link href={"/global-chatter-free-international-calls"}>International Calls</Link></li>
+                                            <li><Link href={"/esim"}>eSIM</Link></li>
+                                        </ul>
+                                    </Col>
+                                </Row>
+                            </Container>
                         </NavDropdown>
                         <Nav.Link href="#">International Calls</Nav.Link>
                         <Nav.Link href="#">Store Locator</Nav.Link>
