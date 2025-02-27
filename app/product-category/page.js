f431cfd6f00b75ae58dacc264fbd03d25b39391d
Kallol Ghosal <kallol@euronoxxgroup.com>
2025-02-27 17:35:19 +0530

    Page issue resolved

--- app/product-category/page.js
+++ app/product-category/page.js
@@ -1,8 +1,8 @@
 "use client"
-import TopHeader from "../../components/TopHeader";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import HeadBar from "../../components/HeadBar";
+import TopHeader from "../components/TopHeader";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import HeadBar from "../components/HeadBar";
 import { Container } from "react-bootstrap";
 
 const ProductCategory = () => {
