4e3f9fb8d21f1339c91ce674584b80aca1d00d55
Kallol Ghosal <kallol@euronoxxgroup.com>
2025-03-21 12:43:23 +0530

    Military Veterans page updated

--- app/military-veterans-form/page.js
+++ app/military-veterans-form/page.js
@@ -7,7 +7,7 @@ import { Button, Col, Container, Form, FormLabel, Row, InputGroup } from "react-
 import React, { useState } from "react";
 import Countrycode from "../products/countrycode.json";
 
-const StudentDiscountForm = () => {
+const MilitaryVeteransForm = () => {
 
     const [errors, setErrors] = useState({});
     const [selectedValue, setSelectedValue] = useState('');
@@ -17,11 +17,12 @@ const StudentDiscountForm = () => {
         countrycode: "",
         phone: "",
         statusproof: "",
-        school: "",
-        yos: "",
-        keepnumber: "",
         plan: "",
         cat: "",
+        famname: "",
+        famemail: "",
+        famphone: "",
+        relation: "",
         concent: false,
         terms: false
     });
@@ -39,18 +40,23 @@ const StudentDiscountForm = () => {
         if (!formData.fname) formErrors.fname = "Your name is required"
         if (!formData.countrycode) formErrors.countrycode = "Your country code is required"
         if (!formData.phone) formErrors.phone = "Phone number is required"
-        if (!formData.school) formErrors.school = "This field is required"
-        if (!formData.yos) formErrors.yos = "Date of birth is required"
         if (!formData.statusproof) formErrors.statusproof = "This field is required"
-        if (!formData.keepnumber) formErrors.keepnumber = "This field is required"
         if (!formData.plan) formErrors.plan = "This field is required"
         if (!formData.cat) formErrors.cat = "This field is required"
+        if (!formData.famname) formErrors.famname = "This field is required"
+        if (!formData.famphone) formErrors.famphone = "This field is required"
+        if (!formData.relation) formErrors.relation = "This field is required"
         if (!formData.concent) formErrors.concent = "This field is required"
         if (!formData.terms) formErrors.terms = "This field is required"
         if (!formData.email) {
             formErrors.email = "Email is required"
         } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
             formErrors.email = "Email address is invalid"
+        }
+        if (!formData.famemail) {
+            formErrors.famemail = "Email is required"
+        } else if (!/\S+@\S+\.\S+/.test(formData.famemail)) {
+            formErrors.famemail = "Email address is invalid"
         }
 
         setErrors(formErrors)
@@ -67,9 +73,9 @@ const StudentDiscountForm = () => {
         <>
         <TopHeader />
         <Header />
-        <HeadBar text={<>Zoiko Mobile College Student Discount Program Registration Form</>} />
+        <HeadBar text={<>Military &amp; Veterans Lifetime Deals Registration Form</>} />
         <Container fluid className="bglite py-5">
-            <Container>
+            <Container className="w-50">
                 <Form onSubmit={handelSubmit}>
                     <Row>
                         <Col md={6} sm={12} xs={12}>
@@ -107,29 +113,10 @@ const StudentDiscountForm = () => {
                         </Col>
                     </Row>
                     <br />
+                    <h4 className="text-center">Select a bring-your-own-device (BYOD) plan that fits your needs</h4>
+                    <p className="text-center">20% Lifetime Discount: Applies to any BYOD plan for the duration of your Zoiko Mobile account.</p>
                     <Row>
                         <Col md={6} sm={12} xs={12}>
-                            <FormLabel htmlFor="school">School Name <span className="txtred">*</span></FormLabel>
-                            <Form.Control type="text" name="school" onChange={handleChange} value={formData.school} placeholder="Name of school" />
-                            {errors.school && <p className="txtred">{errors.school}</p>}
-                        </Col>
-                        <Col md={6} sm={12} xs={12}>
-                            <FormLabel htmlFor="yos">Year of study <span className="txtred">*</span></FormLabel>
-                            <Form.Control type="date" name="yos" onChange={handleChange} value={formData.yos} placeholder="Year of study" />
-                            {errors.yos && <p className="txtred">{errors.yos}</p>}
-                        </Col>
-                    </Row>
-                    <br />
-                    <Row>
-                        <Col md={4} sm={12} xs={12}>
-                            <FormLabel htmlFor="keepnumber">Do you want to keep your current number? <span className="txtred">*</span></FormLabel>
-                            <Form.Select name="keepnumber">
-                                <option value={'yes'}>Yes, I want to keep my number</option>
-                                <option value={'no'}>No, I want a new number</option>
-                            </Form.Select>
-                            {errors.keepnumber && <p className="txtred">{errors.keepnumber}</p>}
-                        </Col>
-                        <Col md={4} sm={12} xs={12}>
                             <FormLabel htmlFor="plan">Select Plan <span className="txtred">*</span></FormLabel>
                             <Form.Select name="plan">
                                 <option value={'prepaid'}>Prepaid</option>
@@ -139,7 +126,7 @@ const StudentDiscountForm = () => {
                             </Form.Select>
                             {errors.plan && <p className="txtred">{errors.plan}</p>}
                         </Col>
-                        <Col md={4} sm={12} xs={12}>
+                        <Col md={6} sm={12} xs={12}>
                             <FormLabel htmlFor="cat">Select Category <span className="txtred">*</span></FormLabel>
                             <Form.Select name="cat">
                                 <option value={'lite'}>Zoiko Lite</option>
@@ -152,9 +139,37 @@ const StudentDiscountForm = () => {
                         </Col>
                     </Row>
                     <br />
-                    <Form.Check label="I hereby declare that the information provided is accurate and complete to the best of my knowledge. I understand that providing false information may result in the termination of services." name="concent" onChange={handleChange} value={formData.concent} type="checkbox" />
+                    <h4 className="text-center pt-5">Add Your Family and Friends(Optional)</h4>
+                    <p className="text-center">Share the benefits! Nominate up to 5 family members or friends to receive a 15% lifetime discount on their BYOD plans.</p>
+                    <Row>
+                        <Col md={6} sm={12} xs={12}>
+                            <FormLabel htmlFor="famname">Full Name <span className="txtred">*</span></FormLabel>
+                            <Form.Control type="text" name="famname" onChange={handleChange} value={formData.famname} placeholder="First name and last name" />
+                            {errors.famname && <p className="txtred">{errors.famname}</p>}
+                        </Col>
+                        <Col md={6} sm={12} xs={12}>
+                            <FormLabel htmlFor="famemail">Eamil <span className="txtred">*</span></FormLabel>
+                            <Form.Control type="email" name="famemail" onChange={handleChange} value={formData.famemail} placeholder="Email" />
+                            {errors.famemail && <p className="txtred">{errors.famemail}</p>}
+                        </Col>
+                    </Row>
+                    <br />
+                    <Row>
+                        <Col md={6} sm={12} xs={12}>
+                            <FormLabel htmlFor="famphone">Contact no <span className="txtred">*</span></FormLabel>
+                            <Form.Control type="text" name="famphone" onChange={handleChange} value={formData.famphone} placeholder="First name and last name" />
+                            {errors.famphone && <p className="txtred">{errors.famphone}</p>}
+                        </Col>
+                        <Col md={6} sm={12} xs={12}>
+                            <FormLabel htmlFor="relation">Relationship <span className="txtred">*</span></FormLabel>
+                            <Form.Control type="text" name="relation" onChange={handleChange} value={formData.relation} placeholder="Relationship" />
+                            {errors.relation && <p className="txtred">{errors.relation}</p>}
+                        </Col>
+                    </Row>
+                    <br />
+                    <Form.Check label="Confirm your selected BYOD plan and verify the details of any nominated family and friends." name="concent" onChange={handleChange} value={formData.concent} type="checkbox" />
                     {errors.concent && <p className="txtred">{errors.concent}</p>}
-                    <Form.Check label="By submitting this form, you agree to Zoiko College Student Discount Program&apos;s terms and conditions ." name="terms" onChange={handleChange} value={formData.terms} type="checkbox" />
+                    <Form.Check label="By submitting this form, you agree to Zoiko Saver Deals' terms and conditions." name="terms" onChange={handleChange} value={formData.terms} type="checkbox" />
                     {errors.terms && <p className="txtred">{errors.terms}</p>}
                     <br />
                     <Button variant="danger" type="submit" name="submit">Submit Your Application</Button>
@@ -165,4 +180,4 @@ const StudentDiscountForm = () => {
         </>
     );
 }
-export default StudentDiscountForm;
+export default MilitaryVeteransForm;
