4b74533edc07adf42699e7b2786521ed7ed60b4c
Kallol Ghosal <kallol@euronoxxgroup.com>
2025-11-03 13:59:02 +0530

    Home banners updated

--- app/components/HomeBanner.js
+++ app/components/HomeBanner.js
@@ -26,8 +26,8 @@ const HomeBanner = () => {
                 <Carousel.Caption>
                     <div className="bannertxt-2 text-start">
                         <h1 className="txtbig txtblack">Fantstic Deals On<br />Refurbished Smartphones<span className="txtred">!</span></h1>
-                        <h2 className="txtblack py-2">Expertly Tested <span className="txtred">|</span> Easy Payment Options</h2>
-                        <h2 className="txtblack pb-2">Premium Quality <span className="txtred">|</span> Free Delivery</h2>
+                        <h2 className="txtblack py-2">Expertly Tested <span className="txtred">|</span> Easy Payment Options<br />
+                        Premium Quality <span className="txtred">|</span> Free Delivery</h2>
                         <div className="d-flex flex-nowrap gap-3">
                             <Button href='#' variant="danger" size="lg">Buy Now</Button>
                             <Button href='#' variant="outline-danger" size="lg">Know More</Button>
@@ -55,9 +55,9 @@ const HomeBanner = () => {
                 <img className="d-sm-block d-md-none" src="/img/home-banner/Postal-Workers-banner-mobile.png" alt="Postal Workers" />
                 <Carousel.Caption>
                     <div className="btnbox text-start">
-                        <h1 className="txtblack">A Special Thank You To<br />U.S. Postal Service Workers</h1>
-                        <h2 className="verybig txtred">20% Discount</h2>
-                        <h1 className="txtblack">On Any Of Our Plans</h1>
+                        <h2 className="txtblack">A Special Thank You To<br />U.S. Postal Service Workers</h2>
+                        <p className="verybig txtred">20% Discount</p>
+                        <h2 className="txtblack">On Any Of Our Plans</h2>
                         <div className="d-flex flex-nowrap gap-3">
                             <Button href='/postal-service-workers-form' variant="danger" size="lg">Register Now</Button>
                             <Button href='#' variant="outline-danger" size="lg">Learn more</Button>
@@ -70,8 +70,7 @@ const HomeBanner = () => {
                 <img className="d-sm-block d-md-none" src="/img/home-banner/Military-Veterans-banner-mobile.png" alt="Military Veterans" />
                 <Carousel.Caption>
                     <div className="btnbox text-start">
-                        <h1 className="bigred">Military &amp; Veterans&apos;</h1>
-                        <h1 className="txtred verybig">Lifetime Deals</h1>
+                        <h1 className="txtred">Military &amp; Veterans&apos;<br />Lifetime Deals</h1>
                         <ul className="redbullet-banner txtblack">
                             <li>20% Lifetime Discount</li>
                             <li>15% Discount For Family &amp; Friends</li>
@@ -88,7 +87,7 @@ const HomeBanner = () => {
                 <img className="d-sm-block d-md-none" src="/img/home-banner/banner-student-mobile.png" alt="Student Banner" />
                 <Carousel.Caption>
                     <div className="btnbox text-start">
-                        <h1 className="txtblack pb-3">Zoiko Mobile College<br />Student Discount Program</h1>
+                        <h2 className="txtred pt-4">Zoiko Mobile Student Discount Program</h2>
                         <h4 className="txtblack">Get Exclusive</h4>
                         <h1 className="txtred verybig">20% Discount</h1>
                         <h1 className="txtblack pb-3">On Any Of Our Plans</h1>
@@ -116,8 +115,7 @@ const HomeBanner = () => {
                 <img className="d-sm-block d-md-none" src="/img/home-banner/Animal-Charities-Banner-mobile.png" alt="Student Banner" />
                 <Carousel.Caption>
                     <div className="btnbox text-start">
-                        <h1 className="txtred bigred">Your Connecton</h1>
-                        <h1 className="txtred verybig">Saves Lives</h1>
+                        <h1 className="txtred bannerhead">Your Connecton<br />Saves Lives</h1>
                         <h2 className="txtblack midbig py-3">Join Zoiko Mobile - We are supporting animals<br />and animal charities whilst keep you connected!</h2>
                         <div className="d-flex flex-nowrap gap-3">
                             <Button href='#' variant="danger" size="lg">Register Now</Button>
@@ -131,8 +129,8 @@ const HomeBanner = () => {
                 <img className="d-sm-block d-md-none" src="/img/home-banner/Music-Hub-banner-mobile.png" alt="Student Banner" />
                 <Carousel.Caption>
                     <div className="btnbox text-start">
-                        <h1 className="txtred bigred">Zoiko Music Hub:</h1>
-                        <h1 className="txtred midbig">Empowering Music Creators and Lovers</h1>
+                        <h1 className="txtred">Zoiko Music Hub:</h1>
+                        <h2 className="txtred">Empowering Music Creators and Lovers</h2>
                         <p className="txtblack body22" style={{fontWeight:'200'}}>At Zoiko Mobile, we know music isn&apos;t just entertainment<br />—it&apos;s a way of life. That&apos;s why we created the Zoiko Music<br />Hub, a dedicated space where music lovers and aspiring<br />musicians can find the tools, perks, and community to fuel<br />their passion. Whether you&apos;re producing your next big hit,<br />jamming with friends, or discovering fresh tunes, Zoiko<br />Mobile is here to keep you connected and inspired.</p>
                         <div className="d-flex flex-nowrap gap-3">
                             <Button href='/music-hub-registratrion-form' variant="danger" size="lg">Register Now</Button>
