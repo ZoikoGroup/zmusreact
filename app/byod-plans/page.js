d4eaa28dc59fdd94f1f76213db867e0ba806e6b8
kallolghosal <kghosal@gmail.com>
2025-11-05 16:01:19 +0530

    BYOD banner updated

--- app/byod-plans/page.js
+++ app/byod-plans/page.js
@@ -58,21 +58,22 @@ const ByodPlans = () => {
       <HeadBar text={<>Bring Your Own Device (BYOD) to Zoiko Mobile</>} />
 
       {/* Banner */}
-      <Container fluid className="byod-banner p-0 position-relative">
-        <div className="position-absolute d-none d-md-block" style={{ left: "11%", bottom: "16%" }}>
-          <DropdownButton variant="danger" size="lg" title="Browse Plans Now">
-            <DropdownItem href="/prepaid-plans">Prepaid Plans</DropdownItem>
-            <DropdownItem href="/postpaid-plans">Postpaid Plans</DropdownItem>
-            <DropdownItem href="/business-deals">Business Deals</DropdownItem>
-          </DropdownButton>
-        </div>
-        <div className="position-absolute d-sm-block d-md-none" style={{ left: "5%", bottom: "5%" }}>
-          <DropdownButton variant="danger" size="sm" title="Browse Plans Now">
-            <DropdownItem href="/prepaid-plans">Prepaid Plans</DropdownItem>
-            <DropdownItem href="/postpaid-plans">Postpaid Plans</DropdownItem>
-            <DropdownItem href="/business-deals">Business Deals</DropdownItem>
-          </DropdownButton>
-        </div>
+      <Container fluid className="p-0">
+        <img className="d-none d-md-block w-100" src="/img/home-banner/byod-banner.png" alt="Student Banner" />
+        <img className="d-sm-block d-md-none" src="/img/home-banner/byod-banner-m.png" alt="Student Banner" />
+        <Container>
+          <div style={{marginTop:'-440px', marginBottom:'200px'}}>
+            <h1 className="d-none d-md-block txtblack w-50">Bring Your Own Device (BYOD) to Zoiko Mobile</h1>
+            <h1 className="d-sm-block d-md-none txtblack">Bring Your Own Device (BYOD) to Zoiko Mobile</h1>
+            <p className="d-none d-md-block body22 w-50">Ready to experience seamless connectivity on Zoiko Mobile&apos;s nationwide network? Bringing your own device is easy! Check the simple steps below to ensure your phone is compatible and ready to go.</p>
+            <p className="d-sm-block d-md-none body22">Ready to experience seamless connectivity on Zoiko Mobile&apos;s nationwide network? Bringing your own device is easy! Check the simple steps below to ensure your phone is compatible and ready to go.</p>
+            <DropdownButton variant="danger" size="lg" title="Browse Plans Now">
+              <DropdownItem href="/prepaid-plans">Prepaid Plans</DropdownItem>
+              <DropdownItem href="/postpaid-plans">Postpaid Plans</DropdownItem>
+              <DropdownItem href="/business-deals">Business Deals</DropdownItem>
+            </DropdownButton>
+          </div>
+        </Container>
       </Container>
 
       {/* Compatibility Info */}
