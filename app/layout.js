7b4bdd8992a6dc6bb7b1431b26eed58e0e0921cc
Anowar Sadat <anowar.vista@gmail.com>
2025-11-28 20:02:04 +0530

    meta pixel

--- app/layout.js
+++ app/layout.js
@@ -760,6 +760,33 @@ export default function RootLayout({ children }) {
             {/* <script id="bp-wa-script" src="https://cdn.botpenguin.com/whatsapp-widget.js">1d7581d3-034a-4279-a26b-c3cac2106314</script> */}
           </>
         )}
+
+
+       {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '838514508768820');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Meta Pixel noscript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=838514508768820&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
 
       <body className={roboto.className}>
