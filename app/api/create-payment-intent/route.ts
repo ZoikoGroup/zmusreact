81ec6c4f1f1da7d282b7d62a9c8b825bfb26db1c
sumonklyn <sumonklyn@gmail.com>
2025-12-15 19:00:18 +0530

    gpay integration4

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -5,11 +5,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST() {
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 1999,
+    amount: 1999, // in cents
     currency: "usd",
-    automatic_payment_methods: {
-      enabled: true, // REQUIRED for GPay / ApplePay
-    },
+    automatic_payment_methods: { enabled: true },
   });
 
   return NextResponse.json({
